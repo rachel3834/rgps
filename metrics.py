fd61c6211bce9134f542418998631d0036fcf989
rachel3834 <rstreet@lco.global>
2025-01-18 19:35:23 -0800

    Added draft code for M1 survey footprint metric

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 
+OPTICAL_COMPONENTS = ['F087', 'F106', 'F129', 'F158', 'F184', 'F213', 'F146', 'G150', 'P127']
 
 def M1_survey_footprint(science_cases, survey_config):
     """
@@ -6,13 +7,16 @@ def M1_survey_footprint(science_cases, survey_config):
     Papers/Science Pitches.
 
     This metric makes use of the CelestialRegion.region_map pre-computed for all science cases.
-    This map of HEALpixel priority for the science in question is compared with the region_map's of the
+    This map of HEALpixel priority for the science in question is compared with the region_maps of the
     survey definition for each optical element requested by the science case.
 
-    The metric value represents the % of region included in survey design’s field per filter.
+    The metric values returned are:
+        percent_map: % of pixels in the desired region included in survey design’s footprint per filter
+        priority_map: % of the summed pixel priority values in the desired region included
+                        in survey design’s footprint per filter
 
     Parameters:
-        science_cases dict   Descriptions of the community-proposed science cases
+        science_cases dict   CelestialRegions representing the community-proposed science cases
         survey_config dict   Description of the proposed survey configuration
 
     Returns:
@@ -21,9 +25,45 @@ def M1_survey_footprint(science_cases, survey_config):
         Output format:
             results = {
                 'survey_concept': {
-                    'optical_element': metric_value
+                    'optical_element': {
+                        'percent_map': array of metric_values for each science case,
+                        'percent_priority': array of metric_values for each science case,
+                        'science_case': list of names of the science cases
+                    }
                 }
             }
     """
 
-    #
+    results = {}
+
+    # For each survey description, loop over all optical components and compare the HEALpixel
+    # maps of the survey footprint with the region map for each science case
+    for survey_name, region_set in survey_config.items():
+
+        # Loop over all optical components since the requested footprints can be different
+        for f in OPTICAL_COMPONENTS:
+            rsurvey = region_set[f]
+
+            # Compare the survey definition with each science case
+            m1 = []
+            m2 = []
+            cases = []
+
+            # This assumes one region per science case per filter
+            for name, info in science_cases.items():
+                cases.append(name)
+
+                # Calculate the number of overlapping pixels, and metric values
+                common_pixels = list(set(info[f].pixels).intersection(set(rsurvey.pixels)))
+
+                m1.append((len(common_pixels) / len(info[f].pixels)) * 100.0)
+                m2.append((info[f][common_pixels].sum() / info[f].sum()) * 100.0)
+
+            results[survey_name][f] = {
+                'percent_map': np.array(m1),
+                'percent_priority': np.array(m2),
+                'science_cases': cases
+            }
+
+    return results
+
